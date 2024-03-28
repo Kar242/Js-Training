e291ccf558192ef172b5fbe0ae5b6b2d5f4289ec
= <kartiktyagi150296@gmail.com>
2024-03-28 18:11:40 +0530

    Task completed

---
+++ index1.js
@@ -0,0 +1,85 @@
+
+//1
+
+function joinTexts(text1, text2) {
+    return text1 + ' ' + text2;
+}
+var joinedTexts = joinTexts("Kartik", "Tyagi");
+console.log(joinedTexts);
+//2
+
+function getTextLength(text) {
+    return text.length;
+}
+var textLength = getTextLength("Kartik");
+console.log(textLength);
+//3
+
+function makeUpperCase(text) {
+    return text.toUpperCase();
+}
+var uppercaseText = makeUpperCase("kartik");
+console.log(uppercaseText);
+
+//4
+function makeLowerCase(text) {
+    return text.toLowerCase();
+}
+var lowercaseText = makeLowerCase("KARTIK");
+console.log(lowercaseText);
+
+//5
+
+function extractSubstring(str, start, length) {
+    return str.substr(start, length);
+}
+var substringExtracted = extractSubstring("Kartik Tyagi", 0, 8);
+console.log(substringExtracted);
+
+//6
+
+function sumNumbers(number1, number2) {
+    return number1 + number2;
+}
+var numbersSummed = sumNumbers(7, 9);
+console.log(numbersSummed);
+
+//7
+
+function minusNumbers(number1, number2) {
+    return number1 - number2;
+}
+var numbersMinused = minusNumbers(15, 8);
+console.log(numbersMinused);
+
+//8
+
+function timesNumbers(number1, number2) {
+    return number1 * number2;
+}
+var numbersTimes = timesNumbers(4, 6);
+console.log(numbersTimes);
+
+//9
+
+function divideNums(number1, number2) {
+    return number1 / number2;
+}
+var numbersDivided = divideNums(20, 5);
+console.log(numbersDivided);
+
+//10
+
+function squareRoot(number) {
+    return Math.sqrt(number);
+}
+var numberSquareRoot = squareRoot(25);
+console.log(numberSquareRoot);
+
+//11
+
+function roundNum(number, places) {
+    return number.toFixed(places);
+}
+var numberRounded = roundNum(2.71828, 3);
+console.log(numberRounded);
